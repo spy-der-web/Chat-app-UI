0e99b1cc03d2ebd2d2dcf0d5cdc48f2792a0cb5a
Joy Obor <oborjoyamara@gmail.com>
2022-01-01 16:22:57 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:chat_app/screens/conversations.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Messenger Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Conversations(),
+    );
+  }
+}
